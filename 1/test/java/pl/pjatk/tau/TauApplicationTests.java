ca5bfdc17fd0492244f03a16a83a32aca2865418
Maciej Leciejewski <s21484@pjwstk.edu.pl>
2022-10-09 13:46:09 +0200

    init

---
+++ 1/test/java/pl/pjatk/tau/TauApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.tau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TauApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
