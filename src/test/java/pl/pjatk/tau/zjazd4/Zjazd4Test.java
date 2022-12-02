85faa3f3fcbe099821d3a35bf4684444576fce32
s21484 <s21484@pjwstk.edu.pl>
2022-12-02 22:00:16 +0100

    zjazd4

---
+++ src/test/java/pl/pjatk/tau/zjazd4/Zjazd4Test.java
@@ -0,0 +1,64 @@
+package pl.pjatk.tau.zjazd4;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pl.pjatk.tau.SomethingToTest;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class Zjazd4Test {
+
+    @Mock
+    private SomethingToTest somethingToTest;
+
+    @Test
+    public void shouldCreateClassInstance() {
+        assertThat(somethingToTest, is(notNullValue()));
+    }
+
+    @Test
+    public void shouldInvokeMethod() {
+        somethingToTest.findTheLowestNumberWhichIsDivisorOfGivenNumbers(1, 2, 3);
+
+        verify(somethingToTest).findTheLowestNumberWhichIsDivisorOfGivenNumbers(1, 2, 3);
+    }
+
+    @Test
+    public void shouldInvokeMethodOneTime() {
+        somethingToTest.calculateSumOfEvenNumbersInGivenRange(1, 10);
+
+        verify(somethingToTest, times(1)).calculateSumOfEvenNumbersInGivenRange(anyInt(), anyInt());
+    }
+
+    @Test
+    public void shouldInvokeMethodWithGivenParamsAtMostOnce() {
+        somethingToTest.calculateSumOfEvenNumbersInGivenRange(1, 10);
+        somethingToTest.calculateSumOfEvenNumbersInGivenRange(1, 5);
+        somethingToTest.calculateSumOfEvenNumbersInGivenRange(2, 7);
+
+        verify(somethingToTest, atMostOnce()).calculateSumOfEvenNumbersInGivenRange(1, 5);
+    }
+
+    @Test
+    public void shouldNeverInvokeMethodWithGivenParams() {
+        somethingToTest.calculateSumOfEvenNumbersInGivenRange(1, 10);
+        somethingToTest.calculateSumOfEvenNumbersInGivenRange(1, 5);
+        somethingToTest.calculateSumOfEvenNumbersInGivenRange(2, 7);
+
+        verify(somethingToTest, never()).calculateSumOfEvenNumbersInGivenRange(1, 100);
+    }
+
+    @Test
+    public void shouldInvokeMethodWithGivenParamsAtLeastOnce() {
+        somethingToTest.calculateSumOfEvenNumbersInGivenRange(1, 10);
+        somethingToTest.calculateSumOfEvenNumbersInGivenRange(1, 5);
+        somethingToTest.calculateSumOfEvenNumbersInGivenRange(1, 7);
+
+        verify(somethingToTest, atLeastOnce()).calculateSumOfEvenNumbersInGivenRange(1, 5);
+    }
+}
